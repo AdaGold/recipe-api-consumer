fbcef375daffe333bc021b6d07db7de574b2a482
jm-rives <joanna.rives@gmail.com>
2016-11-01 08:17:53 -0700

    ADD test controller file

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+# this serves as the controller for my landing page
+
+class Search_Controller < ActionController::Base
+  def index
+
+  end
+
+
+end
---
+++ test/controllers/test_search_controller.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TestSearchController < ActionController::TestCase
+  # test "the truth" do
+  # assert true
+  # end
+end
