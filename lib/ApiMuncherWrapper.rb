821b582c16fad653deec283e0a3ce28a934ed2df
jm-rives <joanna.rives@gmail.com>
2016-11-03 16:46:23 -0700

    ADD partial wrapper and search view

--- lib/ApiMuncherWrapper.rb
+++ lib/ApiMuncherWrapper.rb
@@ -4,10 +4,14 @@ class ApiMuncherWrapper
   BASE_URL = "https://api.edamam.com/search"
   APP_ID = 'APP_ID'
   APP_KEY = 'APP_KEY'
-
-  # from Edamam q = query, r = return
+  
+  # https://developer.edamam.com/edamam-docs-recipe-api
+  # from Edamam q = query text, r = return of recipe based on it's id
   attr_reader :recipe, :q, :url, :image, :returns, :r
   def initialize(recipe, q, url, image, returns, r)
-
+    @recipe = recipe
+    @q = q
+    @url = url
+    @image = image
   end
 end
