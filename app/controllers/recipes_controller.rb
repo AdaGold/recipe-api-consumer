7a606c3d88326e85079655d2aaf2fde24eb6dc99
Lauren <Laurenelee88@gmail.com>
2017-10-31 12:30:52 -0700

    working on show page for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,32 +3,23 @@ require 'recipe'
 require 'muncher_wrapper'
 class RecipesController < ApplicationController
 
-  # perhaps move this to a root homepage controller?
-  # index
   def root ; end
-  # def search
-  #   @recipes = MuncherWrapper.find_recipe(search)
-  # end
-
+  # 1- user will search and it will display a list  of all results
+  # 2- that list view will show 10 recipe names and an image at a time
   def index
     @search = params[:search]
     recipes = MuncherWrapper.find_recipe(@search)
     @recipes = recipes
     # .paginate(:page => params[:page], :per_page => 10)
   end
-
+  # 3- This shall show the details about a given recipe. These details include:
+  # Name
+  # Link to the original recipe (opens in a new tab)
+  # Ingredients
+  # Dietary information
   def show
-    @recipe = MuncherWrapper.show_recipe(params[:uri])
+    @recipe_uri = params[:uri]
+    @recipe = MuncherWrapper.show_recipe(@recipe_uri)
   end
 
 end
-
-# 1- user will search and it will display a list (so index?) of all results
-
-# 2- that list view will show 10 recipe names and an image at a time
-
-# 3- This shall show the details about a given recipe. These details include:
-# Name
-# Link to the original recipe (opens in a new tab)
-# Ingredients
-# Dietary information
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'recipes#root'
-
+  # resources :recipes, only: [:index, :show]
   get 'recipes', to: 'recipes#index', as: 'search_recipe'
-
+  #
   get 'recipes/:label', to: 'recipes#show', as: 'recipe'
 end
 
