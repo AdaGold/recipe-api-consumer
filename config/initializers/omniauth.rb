ced74407ae09ec396ebe88b89632bd2a202dd7a2
Lauren <Laurenelee88@gmail.com>
2017-11-02 12:57:50 -0700

    google oauth for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "310059986027-gt026eq6clp1lr9hq53kmqs60hdqlq89.apps.googleusercontent.com", "1BWLyquxt8zcqWidXa8uZ1ce", {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
