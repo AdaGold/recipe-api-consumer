18fc84e2c2b8f6b699be6cc6793a8184c59a845a
Lauren <Laurenelee88@gmail.com>
2017-11-03 12:48:48 -0700

    test for custom user model method, one failing test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User < ApplicationRecord
 
+  # def self.from_auth_hash(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.name = auth.info.name
+  #     user.oauth_token = auth.info.token
+  #     user.oauth_expires_at = auth.info.expires_at
+  #     user.save!
+  #   end
+  # end
+
   def self.from_auth_hash(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.info.token
-      user.oauth_expires_at = auth.info.expires_at
-      user.save!
-    end
+    user = new
+    user.provider = auth['provider']
+    user.uid = auth['uid']
+    user.name = auth['name']
+    user.oauth_token = auth['token']
+    user.oauth_expires_at = auth['expires_at']
+    user.save!
+    return user
   end
 end
