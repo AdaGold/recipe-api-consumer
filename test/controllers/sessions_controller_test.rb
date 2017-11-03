360daf2db871c1cb579a8b283271bd5c3a4cd136
Lauren <Laurenelee88@gmail.com>
2017-11-02 19:09:41 -0700

    sign out test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,10 +1,25 @@
 require "test_helper"
 
 describe SessionsController do
-  it "logs in a user" do
-    user = users(:ada)
-    login(user)
-    must_redirect_to root_path
-    session[:user_id].must_equal  user.id
+  describe "create" do
+    it "logs in a user" do
+      start_count = User.count
+      user = users(:ada)
+      login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal  user.id
+      User.count.must_equal start_count
+
+    end
+  end
+
+  describe "destroy" do
+    it "logs a user out" do
+      user = users(:ada)
+      login(user)
+      get signout_path(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal  nil
+    end
   end
 end
