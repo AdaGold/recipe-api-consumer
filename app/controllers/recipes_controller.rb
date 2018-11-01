0e160a7ad7dfd8f7a578767bfb5ad98cca5d5836
Katricia Smith <smithkatricia@gmail.com>
2018-11-01 16:13:54 -0700

    Add pseudocode for recipes#index and recipes#show to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,15 @@
 class RecipesController < ApplicationController
+
+  def index
+    # If search is not empty
+    # recipes is the list of recipes from the EdamamAPIWrapper
+    # Show only 10 per page
+    # @recipes
+  end
+
+  def show
+    # Show one recipe details page
+    # @recipe = EdamamWrapper.show_details method 
+  end
+
 end
