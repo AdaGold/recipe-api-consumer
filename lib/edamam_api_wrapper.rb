ed0f7cd16dc96f0869f80ea419234dbe6833ea9b
Katricia Smith <smithkatricia@gmail.com>
2018-11-02 16:01:34 -0700

    Add search method for Edamam Api Wrapper

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,11 +1,26 @@
 require 'httparty'
 
 class EdamamApiWrapper
+  BASE_URL = "https://api.edamam.com/"
+  APP_ID = ENV[EDAMAM_APP_ID]
+  APP_KEYS = ENV[EDAMAM_APP_KEYS]
 
-  def self.search
+  def self.search(search_term)
+    search_url = BASE_URL + "search?" + "q=#{search_term}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEYS}&from=0&to=30"
+
+    # request = HTTParty.get(search_url,
+    #   body: {
+    #     "q" => search_term,
+    #     "app_id" =>
+    #     "app_key"
+    #   },
+    #   headers: {
+    #
+    #   }
+    # )
   end
 
-  def self.show_details
-  end 
+  def self.show_details(recipe_url)
+  end
 
 end
