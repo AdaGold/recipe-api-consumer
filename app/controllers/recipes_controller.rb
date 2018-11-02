81f8cb4c9d2aee5623cf4732f0239ccbec700676
Katricia Smith <smithkatricia@gmail.com>
2018-11-02 12:12:45 -0700

    Add base functions for recipes#index and recipes#show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,17 @@
 class RecipesController < ApplicationController
 
   def index
-    # If search is not empty
-    # recipes is the list of recipes from the EdamamAPIWrapper
-    # Show only 10 per page
-    # @recipes
+    # If search q is not empty
+    if params[:q].present?
+      # recipes is the list of recipes from the EdamamAPIWrapper
+      # Show only 10 per page
+      @recipes = EdamamApiWrapper.search(params[:q])
+    end
   end
 
   def show
-    # Show one recipe details page
-    # @recipe = EdamamWrapper.show_details method 
+    # Show one recipe's details page
+    @recipe = EdamamApiWrapper.show_details(params[:uri])
   end
 
 end
