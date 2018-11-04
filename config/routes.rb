bbcebc4f1b9f7de4bc43a10e4a418fcedb33958b
Katricia Smith <smithkatricia@gmail.com>
2018-11-04 10:40:43 -0800

    Add root route to index and recipe resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipes#index'
+  # root 'recipes#new'
+
+  # can be homepage not attached to controller or must be controller
+  # get 'recipes/search', to: , as: 'search'
+  # get 'recipes/new', to: 'recipes#new', as: 'search'
+
+  # get 'recipes/index', to: 'recipes#index', as: 'index'
+  # get 'recipe', to: 'recipes#show', as: 'show'
+
+  resources :recipes
 end
+
+
+# :recipes/new
+# :recipes/index
