89b96ee8f471a2699f991ea9d53a515d5ed82430
Katricia Smith <smithkatricia@gmail.com>
2018-11-04 10:41:35 -0800

    Add parsing of API response to wrapper search and show details methods

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,26 +1,63 @@
 require 'httparty'
 
 class EdamamApiWrapper
-  BASE_URL = "https://api.edamam.com/"
+  BASE_URL = "https://api.edamam.com/search"
   APP_ID = ENV[EDAMAM_APP_ID]
   APP_KEYS = ENV[EDAMAM_APP_KEYS]
 
   def self.search(search_term)
-    search_url = BASE_URL + "search?" + "q=#{search_term}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEYS}&from=0&to=30"
+    search_url = BASE_URL + "?q=#{search_term}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEYS}&from=0&to=30"
+    response = HTTParty.get(search_url)
+    search_results = []
 
-    # request = HTTParty.get(search_url,
-    #   body: {
-    #     "q" => search_term,
-    #     "app_id" =>
-    #     "app_key"
-    #   },
-    #   headers: {
-    #
-    #   }
-    # )
+    if response["hits"]
+      response["hits"].each do |hit|
+        # move to private method?
+        wrapped_hit = Recipe.new(
+          hit["recipe"]["uri"],
+          hit["recipe"]["label"]
+          hit["recipe"]["image"]
+          hit["recipe"]["source"]
+          hit["recipe"]["url"]
+          hit["recipe"]["yield"]
+          hit["recipe"]["ingredientLines"]
+          hit["recipe"]["healthLabels"]
+        )
+
+        search_results << wrapped_hit
+    end
+
+    return search_results
   end
 
-  def self.show_details(recipe_url)
+
+  def self.show_details(recipe_uri)
+    recipe_url = BASE_URL
+
+    response = HTTParty.get(
+
+    if response
+        # move instantiation to private method?
+        recipe = Recipe.new(
+          response["uri"],
+          response["label"]
+          response["image"]
+          response["source"]
+          response["url"]
+          response["yield"]
+          response["ingredientLines"]
+          response["healthLabels"]
+        )
+
+        return recipe
+    end
+
   end
 
+# private
+#
+#   self.create_recipe(api_params)
+#     return Recipe.new(
+#       hits[recipe][uri]
+#     )
 end
