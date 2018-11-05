01e383b325703e3b82d5b144fc958fd573e47318
Katricia Smith <smithkatricia@gmail.com>
2018-11-04 18:35:51 -0800

    Change Recipe initialization variable recipe_name to name

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,12 +1,12 @@
 class Recipe
-  attr_reader :id, :recipe_name, :image, :source, :url, :servings, :ingredients, :health_labels
+  attr_reader :id, :name, :image, :source, :url, :servings, :ingredients, :health_labels
 
-  def initialize(uri, recipe_name, image, source, url, servings, ingredients, health_labels)
+  def initialize(uri, name, image, source, url, servings, ingredients, health_labels)
   # use keyword args?
 
   # uri is the unique identifier. use as id?
     @id = uri.last(32)
-    @recipe_name = recipe_name
+    @name = name
     @image = image
     @source = source
     @url = url
