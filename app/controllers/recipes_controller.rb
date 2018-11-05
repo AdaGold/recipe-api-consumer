d23db3c7d54a87c7dff277a34b0bd9d6eabc8ac4
Katricia Smith <smithkatricia@gmail.com>
2018-11-04 19:36:02 -0800

    Add pagination to recipes index view and recipes#index method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,6 @@
+require 'will_paginate'
+require 'will_paginate/array'
+
 class RecipesController < ApplicationController
 
   # def new
@@ -8,8 +11,7 @@ class RecipesController < ApplicationController
     if params[:q].present?
       # recipes is the list of recipes from the EdamamAPIWrapper
       # Show only 10 per page
-      @recipes = EdamamApiWrapper.search(params[:q])
-      #.paginate(page: params[:page], per_page: 10)
+      @recipes = EdamamApiWrapper.search(params[:q]).paginate(page: params[:page], per_page: 10)
     end
   end
 
