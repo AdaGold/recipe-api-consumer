4f3c6ddde3fd99d04f7f15c2be001f1911135001
Katricia Smith <smithkatricia@gmail.com>
2018-11-04 18:33:40 -0800

    Comment out paginate method on search results

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,13 +8,14 @@ class RecipesController < ApplicationController
     if params[:q].present?
       # recipes is the list of recipes from the EdamamAPIWrapper
       # Show only 10 per page
-      @recipes = EdamamApiWrapper.search(params[:q]).paginate(page: params[:page], per_page: 10)
+      @recipes = EdamamApiWrapper.search(params[:q])
+      #.paginate(page: params[:page], per_page: 10)
     end
   end
 
   def show
     # Show one recipe's details page
-      @recipe = EdamamApiWrapper.find_details(params[:recipe_id])
+      @recipe = EdamamApiWrapper.find_details(params[:id])
   end
 
 end
